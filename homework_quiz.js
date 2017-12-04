e8969b63d86d831877071efcae90b3951d582930
Marta <martadabrowka@gmail.com>
2017-12-04 19:49:16 +0000

    added fourth answer

--- homework_quiz.js
+++ homework_quiz.js
@@ -1,17 +1,18 @@
- Episode 1
+// Episode 1
 'My name is Keith.'
- Episode 2
+// Episode 2
 3
- Episode 3
+// Episode 3
 'Ducks'
 'Dogs'
 'Lions'
- Episode 4
+// Episode 4
+'Suspects include: Jay, Val, Harvey, Rick'
+'Suspect three is Keith'
+// Episode 5
 
- Episode 5
+// Episode 6
 
- Episode 6
+// Episode 7
 
- Episode 7
-
- Episode 8
+// Episode 8
